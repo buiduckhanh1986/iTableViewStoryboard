27e410c1993436f979de707443f08a8b2dc2e12f
Bui Duc Khanh <buiduckhanh@hos-macbook-pro.local>
2016-11-24 09:13:25 +0700

    Initial Commit

---
+++ iTableViewStoryboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTableViewStoryboard
+//
+//  Created by Bui Duc Khanh on 11/24/16.
+//  Copyright © 2016 Bui Duc Khanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
